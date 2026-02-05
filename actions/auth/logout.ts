76524f8df3ec84d1d2c124fb3a0be255e376f2b4
imran farooq <imran_farooq7@hotmail.com>
2026-02-05 21:23:36 +0500

    logout action added

---
+++ actions/auth/logout.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function logoutAction() {
+  const supabase = await createSupabaseServerClient();
+
+  await supabase.auth.signOut();
+
+  redirect("/login");
+}
